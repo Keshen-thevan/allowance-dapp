dc5163ef9c5d0bd961b5ab88956016266d940695
Keshen <keshenthevan13@gmail.com>
2022-09-09 02:51:05 +0200

    add connection with metamask and get owner functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import Web3Modal from 'web3modal'
+import { providers, Contract, utils } from "ethers"
+import { useRef, useEffect, useState } from "react"
+import { contract_ABI, contract_ADDRESS } from '../src/constants'
+
 
 function App() {
+  const web3ModalRef = useRef();
+  const [walletConnected, setWalletConnected] = useState(false)
+  const [user, setUser] = useState('')
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    const accounts = await window.ethereum.request({
+      method:"eth_requestAccounts"
+    })
+    setUser(accounts[0])
+    console.log(accounts[0])
+
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  const connectWallet = async() =>{
+    try{
+      await getProviderOrSigner()
+      setWalletConnected(true)
+      console.log("connected wallet")
+    }catch(err){console.error(err)}
+  }
+
+  useEffect(() => {
+    if (!walletConnected) {
+
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
+  const getOwner = async() =>{
+    const provider = await getProviderOrSigner();
+    const walletContract = new Contract( contract_ADDRESS,contract_ABI, provider);
+    const _owner = await walletContract.owner()
+    console.log(_owner)
+  } 
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{user}</div>
+      <div>
+        <button onClick={connectWallet}>Connect Wallet</button>
+      </div>
+
+
+      <div>
+        <button onClick={getOwner}>Get Owner</button>
+      </div>
     </div>
   );
 }
